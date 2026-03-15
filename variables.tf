fc8857dbb412661450e8329717112856b8cdbc2e
erikalduaifi-jpg <erik.alduaifi@chasacademy.se>
2026-03-15 19:43:17 +0100

    images

--- variables.tf
+++ variables.tf
@@ -6,7 +6,7 @@ variable "project_id" {
 variable "region" {
   description = "GCP Region"
   type        = string
-  default     = "europe-north1"
+  default     = "europe-west1-b"
 }
 
 variable "student_id" {
